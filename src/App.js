b70047ba6e527a4f79b0550c6526ef0c1e9a0eff
Don-Stevenson <dsteven1@gmail.com>
2020-05-11 19:14:19 -0400

    add objects for each page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,18 @@ class App extends React.Component {
         { title: "Home", path: "/" },
         { title: "about", path: "/about" },
         { title: "Contact", path: "/contact" }
-      ]
+      ],
+      home: {
+        title: "Never Stop Learning",
+        subTitle: "Projects that make a difference",
+        text: 'Checkout my projects below' 
+      },
+      about: {
+        title: 'About me'
+      },
+      contact: {
+        title: 'Talk to me'
+      }
     };
   }
 
