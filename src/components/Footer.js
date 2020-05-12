4cc808559fbe122be560bc9ce91ec0dc578ce016
Don-Stevenson <dsteven1@gmail.com>
2020-05-12 15:02:43 -0400

    set up footer to have site made by

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,14 @@ import Col from "react-bootstrap/Col";
 export default function Footer () {
   return (
     <footer className="mt-5">
-      <Container>
-        <Row>
+      <Container fluid={true}>
+        <Row className="border-top justify-content-between p-3">
           <Col className="p-0" md={3} sm={12}>
             Don Stevenson
+          </Col>
+          <Col className="p-0 d-flex justify-content-end" md={3} >
+              This site was made by Don Stevenson
+          
           </Col>
         </Row>
       </Container>
