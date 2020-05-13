5d514377113e1bb5a3cac7f374325b23a1211e14
Don-Stevenson <dsteven1@gmail.com>
2020-05-13 16:28:30 -0400

    basic setup of carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,1 +1,18 @@
 import React from 'react'
+
+class Carousel extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <p>Carousel here</p>
+        )
+    }
+}
+
+export default Carousel
