6ff3150183050d6584c7d28ae8e63d1c4a7b4b7f
Don-Stevenson <dsteven1@gmail.com>
2020-05-13 16:37:09 -0400

    set up carousel objects

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,7 +5,35 @@ class Carousel extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            items : [
+                {
+                    id: 0,
+                    title : 'Nag-me',
+                    subTitle : 'A nudge theory goal attainment app',
+                    imgSrc: 'from git hub',
+                    link: 'from git hub',
+                    selected: false,
+                },
+                {
+                    id: 1,
+                    title : 'Movie-DB',
+                    subTitle : 'A React based app the returns info on a movie query',
+                    imgSrc: 'from git hub',
+                    link: 'from git hub',
+                    selected: false,
+                },
+                {
+                    id: 3,
+                    title : 'Scheduler',
+                    subTitle : 'A React based app for booking appointments ',
+                    imgSrc: 'from git hub',
+                    link: 'from git hub',
+                    selected: false,
+                },
 
+
+
+            ]
         }
     }
     render() {
