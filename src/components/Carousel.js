608760d7599ca4e0fc9e56ae9ccc3b45cfcfc996
Don-Stevenson <dsteven1@gmail.com>
2020-05-13 16:49:09 -0400

    set up images import

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,46 +1,43 @@
-import React from 'react'
+import React from "react";
+import NagMe from "../components/assets/images/NagTrackerChart.png";
+import MovieDB from "../components/assets/images/batman movieDB.png";
+import Scheduler from "../components/assets/images/Main interface.png";
 
 class Carousel extends React.Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            items : [
-                {
-                    id: 0,
-                    title : 'Nag-me',
-                    subTitle : 'A nudge theory goal attainment app',
-                    imgSrc: 'from git hub',
-                    link: 'from git hub',
-                    selected: false,
-                },
-                {
-                    id: 1,
-                    title : 'Movie-DB',
-                    subTitle : 'A React based app the returns info on a movie query',
-                    imgSrc: 'from git hub',
-                    link: 'from git hub',
-                    selected: false,
-                },
-                {
-                    id: 3,
-                    title : 'Scheduler',
-                    subTitle : 'A React based app for booking appointments ',
-                    imgSrc: 'from git hub',
-                    link: 'from git hub',
-                    selected: false,
-                },
-
-
-
-            ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 0,
+          title: "Nag-me",
+          subTitle: "A nudge theory goal attainment app",
+          imgSrc: NagMe,
+          link: "from git hub",
+          selected: false
+        },
+        {
+          id: 1,
+          title: "Movie-DB",
+          subTitle: "A React based app the returns info on a movie query",
+          imgSrc: MovieDB,
+          link: "from git hub",
+          selected: false
+        },
+        {
+          id: 3,
+          title: "Scheduler",
+          subTitle: "A React based app for booking appointments ",
+          imgSrc: MovieDB,
+          link: "from git hub",
+          selected: false
         }
-    }
-    render() {
-        return(
-            <p>Carousel here</p>
-        )
-    }
+      ]
+    };
+  }
+  render() {
+    return <p>Carousel here</p>;
+  }
 }
 
-export default Carousel
+export default Carousel;
