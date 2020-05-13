5c85210188cdc7f2e1d427ddb5247fc946c28708
Don-Stevenson <dsteven1@gmail.com>
2020-05-13 17:33:02 -0400

    setup cardInfo

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useSpring, animated } from "react-spring";
 
 export default function CardInfo(props) {
-return (
-    <p>Card info</p>
-)
+  const style = useSpring({ opacity: 1, from: { opacity: 0 } });
+  return (
+    <animated.div className="" style={style}>
+      <p>{props.title}</p>
+      <p>{props.subTitle}</p>
+      <a href={props.link} target="_blank" rel="noopener noreferrrer">
+        {" "}
+        View{" "}
+      </a>
+    </animated.div>
+  );
 }
