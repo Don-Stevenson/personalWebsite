9a5881e24bc55781c9768a8efad6b77dd2a660f4
Don-Stevenson <dsteven1@gmail.com>
2020-05-13 16:15:27 -0400

    with working auto year date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,19 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+const year = new Date().toLocaleDateString("en-US", { year: "numeric" });
+console.log(()=> year);
+
 export default function Footer() {
   return (
     <footer className="mt-5">
       <Container fluid={true}>
         <Row className="border-top justify-content-between p-3">
           <Col className="p-0" md={3} sm={12}>
-            Don Stevenson
+            {/* Don Stevenson */}
           </Col>
           <Col className="p-0 d-flex justify-content-end" md={3}>
-            © Don Stevenson
+            © {year} Don Stevenson
           </Col>
         </Row>
       </Container>
