0546ad59db793c2a081c07a2f4386ec293bbab8a
Don-Stevenson <dsteven1@gmail.com>
2020-05-13 16:30:24 -0400

    hook up carousel to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Carousel from "../components/Carousel";
 
 export default function HomePage(props) {
-  return <Hero title={props.title} subTitle={props.subTitle} text={props.text} />;
+  return (
+    <div>
+      <Hero title={props.title} subTitle={props.subTitle} text={props.text} />;
+      <Carousel />
+    </div>
+  );
 }
