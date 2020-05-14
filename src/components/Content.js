5b88844a0dc816ce23e54197429d00bfbcbc040e
Don-Stevenson <dsteven1@gmail.com>
2020-05-14 19:45:46 -0400

    update comments in content js

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,6 +3,9 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+// creates a custom wrapper that we can use elsewhere to wrap and style text
+//**************************************************************************/
+
 export default function Content(props) {
   return (
     <Container fluid={true}>
