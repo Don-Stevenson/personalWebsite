e08eb03dc3ee219de514b2797eb4cd1ba6537149
Don-Stevenson <dsteven1@gmail.com>
2020-05-14 19:44:14 -0400

    setup content container to style elements as we see fit

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-export default function Content() {
-  return <p> Content</p>;
+export default function Content(props) {
+  return (
+    <Container fluid={true}>
+      <Row className="justify-content-center">
+        <Col md={8}>{props.children}</Col>
+      </Row>
+    </Container>
+  );
 }
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,14 +1,17 @@
 import React from "react";
-import Hero from '../components/Hero'
-import Container from "react-bootstrap/Container";
-import Content from '../components/Content'
+import Hero from "../components/Hero";
+import Content from "../components/Content";
 
 export default function AboutPage(props) {
-    return(
-        <Container>
-            <Hero title={props.title}/>
-            <p>Hi, I'm Don Stevenson.  I'm a full stack web developer with experience in Javascript, Ruby on Rails, Node, React, Express and more!  </p>
-        </Container>
-   
-    )
+  return (
+    <div>
+      <Hero title={props.title} />
+      <Content>
+        <p>
+          Hi, I'm Don Stevenson. I'm a full stack web developer with experience
+          in Javascript, Ruby on Rails, Node, React, Express and more!{" "}
+        </p>
+      </Content>
+    </div>
+  );
 }
