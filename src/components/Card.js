486b4a97bfe333798357cb5f3b589c0506932d8a
Don-Stevenson <dsteven1@gmail.com>
2020-05-14 19:26:13 -0400

    fix bug with handleClick

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,13 +5,13 @@ export default function Card(props) {
   return (
     <div className="d-inline-block d-card" onClick={(e)=>props.click(props.item)}>
       <img className="d-card-image" src={props.item.imgSrc} alt={props.item.subTitle} />
-      {props.item.selected && (
+      {props.item.selected && 
         <CardInfo
           title={props.item.title}
           subTitle={props.item.subTitle}
           link={props.item.link}
         />
-      )}
+      }
     </div>
   );
 }
