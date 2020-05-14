74a3a3f6eaac3314b1fad325e4b05afa9566a1de
Don-Stevenson <dsteven1@gmail.com>
2020-05-14 19:34:18 -0400

    update about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Hero from '../components/Hero'
+// import Content from '../components/Content'
 
-export default function AboutPage() {
+export default function AboutPage(props) {
     return(
-        <p>About page </p>
+        <div>
+            <Hero title={props.title}/>
+            <p>Hi, I'm Don Stevenson.  I'm a full stack web developer with experience in Javascript, Ruby on Rails, Node, React, Express and more!  </p>
+
+        </div>
     )
 }
