bd76f8d595df44bfe4557fa9606c320d7d7c2698
Don-Stevenson <dsteven1@gmail.com>
2020-05-15 03:28:07 -0400

    update comments

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,19 +1,27 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Content from "../components/Content";
-import ProfilePic from '../components/assets/images/profile pic.jpg'
+import ProfilePic from "../components/assets/images/profile pic.jpg";
 
 export default function AboutPage(props) {
   return (
     <div>
       <Hero title={props.title} />
-      <img className="d-card-profile" src={ProfilePic} alt='profile' />
       <Content>
+        <img className="d-card-profile" src={ProfilePic} alt="profile" />
+        <br></br>
+        <br></br>
         <p>
           Hi, I'm Don Stevenson. I'm a full stack web developer with experience
-          in Javascript, Ruby on Rails, Node Js, React, Express JS, PostGres and Mocha / Chai.
+          in Javascript, Ruby on Rails, Node Js, React, Express JS, PostGres and
+          Mocha / Chai.
         </p>
-        <p> When I am not building apps and work other coding projects, I like to do practice guitar, play sports, roast my own coffee and do different DIY projects. </p>
+        <p>
+          {" "}
+          When I am not building apps and work other coding projects, I like to
+          do things like: practice guitar, play sports, roast my own coffee and
+          do different DIY projects.{" "}
+        </p>
       </Content>
     </div>
   );
