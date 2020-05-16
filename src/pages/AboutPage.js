b5e96d9bd99b215b796093be08cdb39a53e68fed
Don-Stevenson <dsteven1@gmail.com>
2020-05-16 14:34:52 -0400

    update about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,20 +8,27 @@ export default function AboutPage(props) {
     <div>
       <Hero title={props.title} />
       <Content>
-        <img className="d-card-profile" src={ProfilePic} alt="profile portrait" />
+        <img
+          className="d-card-profile"
+          src={ProfilePic}
+          alt="profile portrait"
+        />
         <br></br>
         <br></br>
         <p>
-          Hi, I'm Don Stevenson. I'm a full stack web developer with experience <br></br>
-          in Javascript, Ruby on Rails, Node Js, React, Express JS, PostGres and  <br></br>
-          Mocha / Chai.
+          Hi, I'm Don Stevenson. I'm a full stack web developer <br></br>
+          with experience in Javascript, Ruby on Rails, Node Js, <br></br>
+          React, Express JS, PostGres and Mocha / Chai.
         </p>
         <p>
           {" "}
-          When I am not building apps and work other coding projects, I like to  <br></br>
-          do things like: practice guitar, play sports, roast my own coffee and  <br></br>
-          do different DIY projects.{" "}
+          When I am not building apps and work other coding <br></br>
+          projects, I like to do things like: practice guitar, <br></br>
+          play sports, roast my own coffee and do different <br></br>
+          DIY projects.{" "}
         </p>
+        <p href={"resume link"}> My Resume </p>
+        <p href={"git hub url"}> My github</p>
       </Content>
     </div>
   );
