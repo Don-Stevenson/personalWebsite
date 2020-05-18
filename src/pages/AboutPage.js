ce7a6e2c00f67c745eca0321d9a2b621a2966b52
Don-Stevenson <dsteven1@gmail.com>
2020-05-18 19:03:31 -0400

    update css

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,6 +5,7 @@ import Content from "../components/Content";
 import Hero from "../components/Hero";
 import ProfilePic from "../components/assets/images/profile-pic.jpg";
 import Resume from "../components/assets/docs/DSresume.pdf";
+import Col from "react-bootstrap/Col";
 
 export default function AboutPage(props) {
   return (
@@ -24,43 +25,38 @@ export default function AboutPage(props) {
     //     </Row>
     // </Column>
 
-    <div>
-      <Content>
-        <Hero title={props.title} />
-        <Row vertical="center">
+    <Column flexGrow={1}>
+      <Hero title={props.title} />
+       <span>
+          <img
+            className="d-card-profile"
+            src={ProfilePic}
+            alt="profile portrait"
+          />
+        </span>
+           </Column>
           <Column flexGrow={1} horizontal="center">
-            <span>
-              <img
-                className="d-card-profile"
-                src={ProfilePic}
-                alt="profile portrait"
-              />
-            </span>
-          </Column>
-          <Column flexGrow={1} horizontal="center">
-            <span>
-              <p>
-                Hi, I'm Don Stevenson. I'm a full stack web developer with
-                experience in Javascript, Ruby on Rails, Node Js, React, Express
-                JS, PostGres and Mocha / Chai.
-              </p>
-              <p>
-                {" "}
-                When I am not building apps and work other coding projects, I
-                enjoy doing things like: practicing guitar, playing sports,
-                roasting my own coffee and making different DIY projects.{" "}
-              </p>
-            </span>
-            <a href={Resume} target="blank">
-              My Resume
-            </a>
-            <a target="blank" href="https://github.com/Don-Stevenson">
-              {" "}
-              My Github
-            </a>
-          </Column>
-        </Row>
-      </Content>
-    </div>
+        <span>
+          <p>
+            Hi, I'm Don Stevenson. I'm a full stack web developer with
+            experience in Javascript, Ruby on Rails, Node Js, React, Express JS,
+            PostGres and Mocha / Chai.
+          </p>
+          <p>
+            {" "}
+            When I am not building apps and work other coding projects, I enjoy
+            doing things like: practicing guitar, playing sports, roasting my
+            own coffee and making different DIY projects.{" "}
+          </p>
+        </span>
+        <a href={Resume} target="blank">
+          My Resume
+        </a>
+        <a target="blank" href="https://github.com/Don-Stevenson">
+          {" "}
+          My Github
+        </a>
+      </Column>
+    
   );
 }
