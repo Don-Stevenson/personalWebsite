c4f6e3f357b1db801fadaea21a0429eb721bf068
Don-Stevenson <dsteven1@gmail.com>
2020-05-19 19:33:22 -0400

    import hero to page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,23 +1,25 @@
 import React from "react";
+import Hero from '../components/Hero'
 
 class ContactPage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            message: '',
-            disabled: false,
-            emailSent: null
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      disabled: false,
+      emailSent: null
+    };
+  }
+  render() {
+    return( <div>
+        <Hero title={this.props.title} />
 
-        }
-    }
-    render(){
-
-        return(
-            <p>Contact page </p>
-        )
-    }
+    </div>
+    )
+  }
 }
 
 export default ContactPage
+;
