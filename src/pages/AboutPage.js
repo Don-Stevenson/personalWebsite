a592b1f806da0249e0139e5b8bf2ad9fc402240d
Don-Stevenson <dsteven1@gmail.com>
2020-05-18 20:35:04 -0400

    update css on about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,42 +1,24 @@
 import React from "react";
 import { Column, Row } from "simple-flexbox";
-import Content from "../components/Content";
 
 import Hero from "../components/Hero";
 import ProfilePic from "../components/assets/images/profile-pic.jpg";
 import Resume from "../components/assets/docs/DSresume.pdf";
-import Col from "react-bootstrap/Col";
+import Content from "../components/Content";
 
 export default function AboutPage(props) {
   return (
-    //       <Column flexGrow={1}>
-    //     <Row horizontal='center'>
-    //         <h1>HEADER</h1>
-    //     </Row>
-    //     <Row vertical='center'>
-    //         <Column flexGrow={1} horizontal='center'>
-    //             <h3> Column 1 </h3>
-    //             <span> column 1 content </span>
-    //         </Column>
-    //         <Column flexGrow={1} horizontal='center'>
-    //             <h3> Column 2 </h3>
-    //             <span> column 2 content </span>
-    //         </Column>
-    //     </Row>
-    // </Column>
-
-    <Column flexGrow={1}>
+    <Content>
       <Hero title={props.title} />
-       <span>
+      <Row>
+        <Column flexGrow={1} horizontal="center">
           <img
             className="d-card-profile"
             src={ProfilePic}
             alt="profile portrait"
           />
-        </span>
-           </Column>
-          <Column flexGrow={1} horizontal="center">
-        <span>
+        </Column>
+        <Column flexGrow={1} horizontal="center">
           <p>
             Hi, I'm Don Stevenson. I'm a full stack web developer with
             experience in Javascript, Ruby on Rails, Node Js, React, Express JS,
@@ -48,15 +30,16 @@ export default function AboutPage(props) {
             doing things like: practicing guitar, playing sports, roasting my
             own coffee and making different DIY projects.{" "}
           </p>
-        </span>
-        <a href={Resume} target="blank">
-          My Resume
-        </a>
-        <a target="blank" href="https://github.com/Don-Stevenson">
-          {" "}
-          My Github
-        </a>
-      </Column>
-    
+
+          <a href={Resume} target="blank">
+            My Resume
+          </a>
+          <a target="blank" href="https://github.com/Don-Stevenson">
+            {" "}
+            My Github
+          </a>
+        </Column>
+      </Row>
+    </Content>
   );
 }
