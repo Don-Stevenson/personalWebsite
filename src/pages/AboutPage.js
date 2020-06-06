57127630ccb309c2bd76cfcb4d4b51d8abb939d1
Don-Stevenson <dsteven1@gmail.com>
2020-06-06 00:54:34 -0400

    add a div to fix title alignment

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,57 +7,58 @@ import ProfilePic from "../components/assets/images/profile-pic.jpg";
 import Resume from "../components/assets/docs/DSresume.pdf";
 import Content from "../components/Content";
 
-
 // returns the about page
 //**********************/
 export default function AboutPage(props) {
   return (
-    <Content>
+    <div>
       <Hero title={props.title}> title </Hero>
-      <Row flexGrow={1}>
-        <Column flexGrow={1} horizontal="center">
-          <img
-            className="d-card-profile"
-            src={ProfilePic}
-            alt="profile portrait"
-          />
-        </Column>
-        <Content>
-          <Column flexGrow={1} horizontal="start">
-            <p>
-              Hi, I'm Don. I'm a full stack web developer with experience in
-              Javascript, Ruby on Rails, Node JS, React, Express JS, SQl,
-              PostgreSQL, HTML5, CSS, Jest, and Mocha / Chai.
-            </p>
-            <p>
-              {" "}
-              When I am not building apps and work other coding projects, I
-              enjoy doing things like: practicing guitar, playing sports,
-              roasting my own coffee and making different DIY projects.{" "}
-            </p>
-
-            <a href={Resume} target="blank" rel="noopener noreferrrer">
-              Here is my resume
-            </a>
-            <br></br>
-            <Row>
-              <SocialIcon
-                url="https://github.com/Don-Stevenson"
-                style={{ margin: 5 }}
-                target="blank"
-                rel="noopener noreferrrer"
-              />
-
-              <SocialIcon
-                url="https://www.linkedin.com/in/don-stevenson-5b31aa128/"
-                style={{ margin: 5 }}
-                target="blank"
-                rel="noopener noreferrrer"
-              />
-            </Row>
+      <Content>
+        <Row flexGrow={1}>
+          <Column flexGrow={1} horizontal="center">
+            <img
+              className="d-card-profile"
+              src={ProfilePic}
+              alt="profile portrait"
+            />
           </Column>
-        </Content>
-      </Row>
-    </Content>
+          <Content>
+            <Column flexGrow={1} horizontal="start">
+              <p>
+                Hi, I'm Don. I'm a full stack web developer with experience in
+                Javascript, Ruby on Rails, Node JS, React, Express JS, SQl,
+                PostgreSQL, HTML5, CSS, Jest, and Mocha / Chai.
+              </p>
+              <p>
+                {" "}
+                When I am not building apps and work other coding projects, I
+                enjoy doing things like: practicing guitar, playing sports,
+                roasting my own coffee and making different DIY projects.{" "}
+              </p>
+
+              <a href={Resume} target="blank" rel="noopener noreferrrer">
+                Here is my resume
+              </a>
+              <br></br>
+              <Row>
+                <SocialIcon
+                  url="https://github.com/Don-Stevenson"
+                  style={{ margin: 5 }}
+                  target="blank"
+                  rel="noopener noreferrrer"
+                />
+
+                <SocialIcon
+                  url="https://www.linkedin.com/in/don-stevenson-5b31aa128/"
+                  style={{ margin: 5 }}
+                  target="blank"
+                  rel="noopener noreferrrer"
+                />
+              </Row>
+            </Column>
+          </Content>
+        </Row>
+      </Content>
+    </div>
   );
 }
