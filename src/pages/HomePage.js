0a5dbc0621a906b11dd77e1874d72f009c85cd36
Don-Stevenson <dsteven1@gmail.com>
2020-06-06 13:43:49 -0400

    update comments, minor css, update export default export function location

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "../components/Hero";
 import Carousel from "../components/Carousel";
 
+// displays the homepage
+//**********************
 export default function HomePage(props) {
   return (
     <div>
