10f0c362199286a4b1f694eace874d6f9bc97536
Don-Stevenson <dsteven1@gmail.com>
2020-11-23 17:45:35 -0500

    update footer to say this site made by don stevenson

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 // displaying the year based on new Date method
-//********************************************
+//*********************************************
 const year = new Date().toLocaleDateString("en-US", { year: "numeric" });
 
 // returns the footer
@@ -18,7 +18,7 @@ export default function Footer() {
             {/* Don Stevenson Web Developer */}
           </Col>
           <Col className="p-0 d-flex justify-content-end" md={3}>
-            © {year} Don Stevenson
+             This site made by Don Stevenson © {year}
           </Col>
         </Row>
       </Container>
