a2b701b1c2e406018d088911dbd8b951eca110e7
Don-Stevenson <dsteven1@gmail.com>
2020-11-23 17:26:18 -0500

    comment out don stevenson web dev

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
       <Container fluid={true}>
         <Row className="border-top justify-content-between p-3">
           <Col className="p-0" md={3} sm={12}>
-            Don Stevenson Web Developer
+            {/* Don Stevenson Web Developer */}
           </Col>
           <Col className="p-0 d-flex justify-content-end" md={3}>
             © {year} Don Stevenson
