48126bbaec3970c1df507170a60845c110deeed2
Don-Stevenson <dsteven1@gmail.com>
2020-11-23 17:22:03 -0500

    update code formating

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,22 @@ import CardInfo from "../components/CardInfo";
 
 export default function Card(props) {
   return (
-    <div className="d-inline-block d-card" onClick={(e)=>props.click(props.item)}>
-      <img className="d-card-image" src={props.item.imgSrc} alt={props.item.subTitle} />
-      {props.item.selected && 
+    <div
+      className="d-inline-block d-card"
+      onClick={(e) => props.click(props.item)}
+    >
+      <img
+        className="d-card-image"
+        src={props.item.imgSrc}
+        alt={props.item.subTitle}
+      />
+      {props.item.selected && (
         <CardInfo
           title={props.item.title}
           subTitle={props.item.subTitle}
           link={props.item.link}
         />
-      }
+      )}
     </div>
   );
 }
