823bf1073a98f3cc52ff872b85cadb1c86ebb026
Don-Stevenson <dsteven1@gmail.com>
2020-12-10 14:25:22 -0500

    update comments

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import Carousel from "../components/Carousel";
 
-// displays the homepage
+// function displays the homepage
 //**********************
 export default function HomePage(props) {
   return (
