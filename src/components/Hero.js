44ae888fc3a1d7495b4515e39096ca9b48e8e5d1
Don-Stevenson <dsteven1@gmail.com>
2021-02-05 20:58:54 -0500

    destructure props into title, subTitle, text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,20 +7,20 @@ import Col from "react-bootstrap/Col";
 // function that returns the homepage title, subtitle and text
 // ***********************************************************
 
-export default function Hero(props) {
+export default function Hero({title, subTitle, text}) {
   return (
     <Jumbotron className="bg-transparent jumbotron-fluid p-0">
       <Container fluid={true}>
         <Row className="justify-content-center py-5">
           <Col md={8} sm={12}>
-            {props.title && (
-              <h1 className="display-1 font-weight-bolder">{props.title}</h1>
+            {title && (
+              <h1 className="display-1 font-weight-bolder">{title}</h1>
             )}
-            {props.subTitle && (
-              <h3 className="display-4 font-weight-light">{props.subTitle}</h3>
+            {subTitle && (
+              <h3 className="display-4 font-weight-light">{subTitle}</h3>
             )}
-            {props.text && (
-              <h3 className="lead font-weight-light">{props.text}</h3>
+            {text && (
+              <h3 className="lead font-weight-light">{text}</h3>
             )}
           </Col>
         </Row>
