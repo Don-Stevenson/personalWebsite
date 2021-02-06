e0e47c0ea9255d3bda5acb54a396d3047891ec89
Don-Stevenson <dsteven1@gmail.com>
2021-02-05 21:17:34 -0500

    destructure props into children

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,11 +6,11 @@ import Col from "react-bootstrap/Col";
 // creates a custom wrapper that we can use elsewhere to wrap and style text
 // **************************************************************************/
 
-export default function Content(props) {
+export default function Content({ children }) {
   return (
     <Container fluid={true}>
       <Row className="justify-content-center">
-        <Col md={8}>{props.children}</Col>
+        <Col md={8}>{children}</Col>
       </Row>
     </Container>
   );
