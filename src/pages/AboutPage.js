2780f181757b18a87cbde6a849f4d579ef7976e4
Don Stevenson <dstevenson@tribalscale.com>
2022-07-23 15:40:20 -0500

    remove simple flexbox

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Column, Row } from "simple-flexbox";
 import { SocialIcon } from "react-social-icons";
 
 import Hero from "../components/Hero";
@@ -14,12 +13,14 @@ export default function AboutPage(props) {
     <div>
       <Hero title={props.title}> title </Hero>
       <Content>
-        <Column wrap={true} vertical={"spaced"}>
+        <div> Column
+
+        {/* <Column wrap={true} vertical={"spaced"}> */}
           <img
             className="d-card-profile"
             src={ProfilePic}
             alt="profile portrait"
-          />
+            />
           <br></br>
           <p>
             Hi, I'm Don! I'm a full stack web developer with experience in
@@ -42,21 +43,24 @@ export default function AboutPage(props) {
           
 
           <p> You can also find me here: {"  "} </p>
-          <Row>
+            <div> row 
+
             <SocialIcon
               url="https://github.com/Don-Stevenson"
               style={{ margin: 6 }}
               target="blank"
               rel="noopener noreferrrer"
-            />
+              />
             <SocialIcon
               url="https://www.linkedin.com/in/don-stevenson-5b31aa128/"
               style={{ margin: 6 }}
               target="blank"
               rel="noopener noreferrrer"
-            />
-          </Row>
-        </Column>
+              />
+          
+              </div>
+        {/* </Column> */}
+              </div>
       </Content>
     </div>
   );
