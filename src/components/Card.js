bb6e4cf6121cf87981daba6374c303ad0703d715
Don Stevenson <dstevenson@tribalscale.com>
2022-11-14 23:13:36 -0500

    Refactor props to click and item

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,20 @@
 import React from "react"
 import CardInfo from "../components/CardInfo"
 
-export default function Card(props) {
+export default function Card({ click, item }) {
   return (
-    <div
-      className="d-inline-block d-card"
-      onClick={() => props.click(props.item)}
-    >
-      <img
-        className="d-card-image"
-        src={props.item.imgSrc}
-        alt={props.item.subTitle}
-      />
+    <div className="d-inline-block d-card" onClick={() => click(item)}>
+      <img className="d-card-image" src={item.imgSrc} alt={item.subTitle} />
 
-      {props.item.selected && (
+      {item.selected && (
         <CardInfo
-          title={props.item.title}
-          subTitle={props.item.subTitle}
-          link={props.item.link}
+          title={item.title}
+          subTitle={item.subTitle}
+          link={item.link}
         />
       )}
 
-      {!props.item.selected && <div className="placeholder"></div>}
+      {!item.selected && <div className="placeholder"></div>}
     </div>
   )
 }
