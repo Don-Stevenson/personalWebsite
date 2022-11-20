4599d014bdbc4e31898a03c816e5204313775113
Don Stevenson <dstevenson@tribalscale.com>
2022-11-20 15:42:09 -0500

    Add Keep Learning test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import { render, prettyDOM, screen } from "@testing-library/react"
+import App from "./App"
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("renders 'Keep Learning.' on the home page", () => {
+  const { getByText } = render(<App />)
+  const titleElement = getByText(/Keep Learning./i)
+  expect(titleElement).toBeInTheDocument()
+})
