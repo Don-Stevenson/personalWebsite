bc93d59adef19796ac7f7f8ea7bb0370b5a9a8d0
PandaComp <pantonio2000@gmail.com>
2023-04-16 14:50:53 -0400

    refactor styles to styles.js

---
+++ src/components/Hero/style.js
@@ -0,0 +1,7 @@
+
+import styled from "styled-components";
+
+export const Jumbotron = styled.div`padding:4 rem;`
+export const Container = styled.div`padding:4 rem;`
+export const Row = styled.div`padding:4 rem;`
+export const Col = styled.div`padding:4 rem;`
