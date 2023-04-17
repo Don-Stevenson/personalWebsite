18a2eaae1f681c6bccf3295ada02e9300243cf0c
Don Stevenson <dstevenson@tribalscale.com>
2023-04-16 20:30:41 -0400

    Clean up unused style classnames

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,26 +1,19 @@
 import React from "react"
-import { Jumbotron, Container, Row, Col } from "./style"
-
-
-
+import { Container, Row, Col, Jumbotron } from "./style"
 
 // function that returns the homepage title, subtitle and text
 // ***********************************************************
 
 export default function Hero({ title, subTitle, text }) {
   return (
-    <Jumbotron className="bg-transparent jumbotron-fluid p-0">
-      <Container fluid={true}>
-        <Row className="justify-content-center py-5">
-          <Col md={8} sm={12}>
-            {title && <h1 className="display-1 font-weight-bolder">{title}</h1>}
-            {subTitle && (
-              <h3 className="display-4 font-weight-light">{subTitle}</h3>
-            )}
-            {text && <h3 className="lead font-weight-light">{text}</h3>}
-          </Col>
-        </Row>
-      </Container>
-    </Jumbotron>
+    <Container>
+      <Row>
+        <Col>
+          {title && <Jumbotron>{title}</Jumbotron>}
+          {subTitle && <h3>{subTitle}</h3>}
+          {text && <h3>{text}</h3>}
+        </Col>
+      </Row>
+    </Container>
   )
 }
