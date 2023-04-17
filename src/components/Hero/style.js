03be62ff89edb9d46647f4f531d4a89f93f079dd
Don Stevenson <dstevenson@tribalscale.com>
2023-04-16 20:31:33 -0400

    Add Jumbotron styles

--- src/components/Hero/style.js
+++ src/components/Hero/style.js
@@ -1,7 +1,21 @@
+import styled from "styled-components"
 
-import styled from "styled-components";
-
-export const Jumbotron = styled.div`padding:4 rem;`
-export const Container = styled.div`padding:4 rem;`
-export const Row = styled.div`padding:4 rem;`
-export const Col = styled.div`padding:4 rem;`
+export const Jumbotron = styled.div`
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    Helvetica Neue, Arial, Noto Sans, Liberation Sans, sans-serif,
+    Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+  font-weight: bolder !important;
+  tex-aling: left;
+  font-size: 6rem;
+  padding: 4 rem;
+  color: #212529;
+`
+export const Container = styled.div`
+  padding: 4 rem;
+`
+export const Row = styled.div`
+  padding: 4 rem;
+`
+export const Col = styled.div`
+  padding: 4 rem;
+`
