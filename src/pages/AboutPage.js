1e4b34a6a8f47656d18aa62a048ad17e5c2e5f51
Don Stevenson <dstevenson@tribalscale.com>
2023-08-27 19:35:12 -0400

    Update imports

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,14 +1,17 @@
 import React from "react"
-import styled from "styled-components";
+import styled from "styled-components"
 import { SocialIcon } from "react-social-icons"
-
 import Hero from "../components/Hero"
 import ProfilePic from "../components/assets/images/profile pic 2024.webp"
 import Resume from "../components/assets/docs/DS Resume.pdf"
 import Content from "../components/Content"
 
-const Column = styled.div`padding: 3rem`
-const Row = styled.div`padding: 3rem`
+const Column = styled.div`
+  padding: 3rem;
+`
+const Row = styled.div`
+  padding: 3rem;
+`
 
 // returns the about page
 //**********************/
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Hero from "../components/Hero"
+import Hero from "../components/Hero/Hero"
 import Carousel from "../components/Carousel"
 
 // function displays the homepage
