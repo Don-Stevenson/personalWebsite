8b11a324226ae98e0098ed1e95636f3939dc27b3
Don Stevenson <dstevenson@tribalscale.com>
2023-08-29 14:05:46 -0400

    Refactor card component styles

---
+++ src/components/Card/Card.js
@@ -0,0 +1,21 @@
+import React from "react"
+import CardInfo from "../CardInfo"
+import { CardWrapper } from "./style"
+
+export default function Card({ click, item }) {
+  return (
+    <CardWrapper onClick={() => click(item)}>
+      <img className="d-card-image" src={item.imgSrc} alt={item.subTitle} />
+
+      {item.selected && (
+        <CardInfo
+          title={item.title}
+          subTitle={item.subTitle}
+          link={item.link}
+        />
+      )}
+
+      {!item.selected && <div className="placeholder"></div>}
+    </CardWrapper>
+  )
+}
