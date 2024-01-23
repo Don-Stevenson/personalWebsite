d13a95ea76b1573d26974b9f36ac4dce628e4d2e
Don Stevenson <dstevenson@tribalscale.com>
2024-01-22 23:23:07 -0500

    Add profile pic, footer, and hero

---
+++ src/components/Footer/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+export const Container = styled.div`
+  display: flex;
+  justify-content: end;
+  margin-top: 16px;
+`
+export const CopyrightContainer = styled.div`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  font-size: 1.2rem;
+`
+
+export const HrLine = styled.hr`
+  border-top: 1px solid lightgrey;
+`
