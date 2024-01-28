f397ad30f2fdbaace50f1149406ea9cd5472a095
Don Stevenson <dstevenson@tribalscale.com>
2024-01-27 20:32:07 -0500

    WIP: refator navbar

--- src/App.js
+++ src/App.js
@@ -5,48 +5,27 @@ import Footer from "./components/Footer/Footer"
 import HomePage from "./pages/HomePage"
 import AboutPage from "./pages/AboutPage"
 import ContactPage from "./pages/ContactPage"
+import Navbar from "./components/NavBar/NavBar"
 
 const Container = styled.div`
   margin: 2rem;
   padding: 2rem;
 `
-const Navbar = styled.div`
-  margin: 2rem;
-  padding: 2rem;
-`
-const Nav = styled.div`
-  margin: 2rem;
-  padding: 2rem;
-`
+
 const App = () => {
   const [home] = useState({
     title: "Keep Learning.",
     subTitle: "Projects that make a difference",
-    text: "Checkout my work below",
+    text: "Checkout my work below"
   })
   const [about] = useState({ title: "About Me." })
   const [contact] = useState({ title: "Talk to Me." })
 
   return (
     <Router>
+      <Navbar />
       <Container className="p-0" fluid={true}>
-        <Navbar className="border-bottom" bg="transparent" expand="lg">
-          <Navbar>Don Stevenson - Web Developer</Navbar>
-          <Navbar className="border-0" aria-controls="navbar-toggle" />
-          <Navbar id="navbar-toggle">
-            <Nav className="ml-auto">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
-            </Nav>
-          </Navbar>
-        </Navbar>
+        <Navbar />
         <Route
           path="/"
           exact
