63bf161e414cedd54749e759d48a7ba2c74d3e3e
Don Stevenson <dstevenson@tribalscale.com>
2024-01-30 22:32:31 -0500

    Update navbar styles

--- src/components/NavBar/Menu/Menu.js
+++ src/components/NavBar/Menu/Menu.js
@@ -1,20 +1,13 @@
 import React from "react"
 import { bool } from "prop-types"
-import { StyledMenu, link } from "./style"
-import { BrowserRouter as Link } from "react-router-dom"
+import { StyledMenu, Link } from "./style"
 
 const Menu = ({ open }) => {
   return (
     <StyledMenu open={open}>
-      <Link className="link" to="/">
-        Home
-      </Link>
-      <Link className="link" to="/about">
-        About
-      </Link>
-      <Link className="link" to="/contact">
-        Contact
-      </Link>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/contact">Contact</Link>
     </StyledMenu>
   )
 }
