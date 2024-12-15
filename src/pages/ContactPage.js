9728f3237dd40b637275fcb6997ce725c68e3c9f
Don Stevenson <dstevenson@tribalscale.com>
2024-12-15 11:01:26 -0500

    -Update and refactor Contact Page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,76 +1,124 @@
 import React from "react"
 import styled from "styled-components"
 import Hero from "../components/Hero/Hero"
-import Content from "../components/Content"
 import Useform from "../../src/UseForm"
-const Form = styled.div`
-  padding: 4 rem;
+import { devices } from "../utils/constants"
+
+const PageContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 `
-const Button = styled.div`
-  padding: 4 rem;
+
+const ContentWrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+`
+
+const FormBox = styled.textarea`
+  border-radius: 4px;
+  border: 1px solid #ced4da;
+  width: 100%;
+`
+
+const Success = styled.p`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  font-size: 0.8rem;
+  color: #9ad19a;
+`
+
+const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 16rem;
+  margin-bottom: 1rem;
+  @media ${devices.sm} {
+    width: 25rem;
+  }
+  @media ${devices.md} {
+    width: 45rem;
+  }
+`
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  border: 1.5px solid;
+  color: #007bff;
+  padding: 5px;
+  border-color: #007bff;
+  border-radius: 4px;
+  width: 3rem;
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`
+
+const Wrapper = styled.div`
+  margin-right: 6rem;
+  margin-left: 2.5rem;
 `
 
 export default function ContactPage({ title }) {
   const { handleSubmit, handleChange, state, errors } = Useform()
 
   return (
-    <div>
+    <PageContainer>
       <Hero title={title} />
-      <Content>
-        <Form onSubmit={handleSubmit}>
-          <Form>
-            <Form htmlFor="full-name">Name</Form>
-            <Form
-              id="full-name"
-              name="name"
-              type="text"
-              value={state.name}
-              onChange={handleChange}
-              required
-            />
-          </Form>
-          {errors.name && <p>{errors.blank}</p>}
+      <ContentWrapper>
+        <Wrapper>
+          <form onSubmit={handleSubmit}>
+            <FormWrapper>
+              <label htmlFor="full-name">Name</label>
+              <FormBox
+                id="full-name"
+                name="name"
+                type="text"
+                value={state.name}
+                onChange={handleChange}
+                required
+              />
+            </FormWrapper>
+            <FormWrapper>
+              <label htmlFor="email">Email</label>
+              <FormBox
+                id="email"
+                name="email"
+                type="email"
+                value={state.email}
+                onChange={handleChange}
+                required
+              />
+            </FormWrapper>
+            <FormWrapper>
+              <label htmlFor="message">Message</label>
+              <FormBox
+                id="message"
+                name="message"
+                rows="3"
+                value={state.message}
+                onChange={handleChange}
+                required
+              />
+            </FormWrapper>
 
-          <Form>
-            <Form htmlFor="email">Email</Form>
-            <Form
-              id="email"
-              name="email"
-              type="email"
-              value={state.email}
-              onChange={handleChange}
-              required
-            />
-          </Form>
+            {errors.message && <p>{errors.message}</p>}
 
-          <Form>
-            <Form htmlFor="message">Message</Form>
-            <Form
-              id="message"
-              name="message"
-              as="textarea"
-              rows="3"
-              value={state.message}
-              onChange={handleChange}
-              required
-            />
-          </Form>
-          {errors.message && <p>{errors.blank}</p>}
-          <p>Note: please allow up to 10 seconds for the email to send</p>
-          <Button
-            className="d-inline-block"
-            variant="outline-primary"
-            type="submit"
-            disabled={state.disabled}
-          >
-            Send
-          </Button>
+            <p>Note: please allow up to 10 seconds for the email to send</p>
 
-          {state.emailSent && (
-            <p className="d-inline d-sucess-msg"> Email Sent!</p>
-          )}
-        </Form>
-      </Content>
-    </div>
+            <Button type="submit" disabled={state.disabled}>
+              Send
+            </Button>
+
+            {state.emailSent && <Success>Email Sent!</Success>}
+          </form>
+        </Wrapper>
+      </ContentWrapper>
+    </PageContainer>
   )
 }
