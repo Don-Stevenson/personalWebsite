4caf2a6e290b641f45fc1225582a339e62a98c6b
Don Stevenson <dstevenson@tribalscale.com>
2025-01-13 00:31:56 -0500

    - update dependencies - add ajv dependency - refactor about page - remove react social icons - add react icons

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import { SocialIcon } from "react-social-icons"
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
 import Hero from "../components/Hero/Hero"
 import ProfilePic from "../components/assets/images/profile pic 2024.webp"
 import Resume from "../components/assets/docs/DS Resume.pdf"
@@ -12,98 +12,131 @@ const PageContainer = styled.div`
   flex-direction: column;
 `
 
-const TextAndIconWrapper = styled.div`
-  // margin-left: 3rem;
-  // padding: 0 3rem;
-`
-const Row = styled.div`
-  @media ${devices.md} {
-    padding: 3 0rem;
-    margin-left: 3rem;
-  }
-`
-
-const Text = styled.p`
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
-  font-size: 1rem;
-  max-width: 30rem;
-  @media ${devices.md} {
-    margin-left: 3rem;
-  }
-`
-
-const ImageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  max-width: 100%;
-`
-
-const StyledProfileImage = styled.img`
-  width: 100%;
-  height: auto;
-  max-width: 400px;
-  object-fit: contain;
-  align-self: left;
-`
-const Wrapper = styled.div`
+const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 2rem;
   margin-bottom: 8rem;
+
   @media ${devices.md} {
     margin-left: 3rem;
     flex-direction: row;
   }
 `
 
+const ImageSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 93%;
+`
+
+const ProfileImage = styled.img`
+  width: 100%;
+  height: auto;
+  max-width: 400px;
+  object-fit: contain;
+  align-self: left;
+`
+
+const ContentSection = styled.div`
+  margin-right: 1rem;
+`
+
+const Paragraph = styled.p`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  font-size: 1rem;
+  max-width: 30rem;
+
+  @media ${devices.md} {
+    margin-top: 0;
+    margin-left: 3rem;
+  }
+`
+
+const SocialLinksRow = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media ${devices.md} {
+    margin-left: 3rem;
+  }
+`
+
+const IconLink = styled.a`
+  text-decoration: none;
+  cursor: pointer;
+`
+
+const StyledGithubIcon = styled(AiFillGithub)`
+  color: #333;
+  font-size: 35px;
+  margin: 6px;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: #666;
+  }
+`
+
+const StyledLinkedinIcon = styled(AiFillLinkedin)`
+  color: #0077b5;
+  font-size: 35px;
+  margin: 6px;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: #0099d4;
+  }
+`
+
 export default function AboutPage({ title }) {
   return (
     <PageContainer>
-      <Hero title={title}> title </Hero>
-      <Wrapper>
-        <ImageWrapper>
-          <StyledProfileImage src={ProfilePic} alt="profile portrait" />
-        </ImageWrapper>
-        <TextAndIconWrapper>
-          <Text>
+      <Hero title={title}>{title}</Hero>
+      <ContentWrapper>
+        <ImageSection>
+          <ProfileImage src={ProfilePic} alt="profile portrait" />
+        </ImageSection>
+        <ContentSection>
+          <Paragraph>
             Hi, I'm Don! I'm a full stack software developer with experience in
             Javascript, Clojure, WordPress, Ruby on Rails, Python, PhP, Node JS,
             React JS, Gatsby JS, Express JS, SQl, GraphQL, PostgreSQL, MongoDB,
             HTML5, CSS, Jest, and Mocha / Chai.
-          </Text>
-
-          <Text>
+          </Paragraph>
+          <Paragraph>
             When I am not building apps and working on other coding projects, I
             enjoy doing things like: practicing guitar, playing team sports,
             keeping aquariums, roasting my own coffee, knife sharpening and
             working on different DIY projects.
-          </Text>
-          <Text>
+          </Paragraph>
+          <Paragraph>
             Here is my{" "}
             <a href={Resume} target="blank" rel="noopener noreferrrer">
               resume
             </a>
-          </Text>
-
-          <Text> You can also find me here: {"  "} </Text>
-          <Row>
-            <SocialIcon
-              url="https://github.com/Don-Stevenson"
-              style={{ margin: 6 }}
-              target="blank"
-              rel="noopener noreferrrer"
-            />
-            <SocialIcon
-              url="https://www.linkedin.com/in/don-stevenson-5b31aa128/"
-              style={{ margin: 6 }}
-              target="blank"
-              rel="noopener noreferrrer"
-            />
-          </Row>
-        </TextAndIconWrapper>
-      </Wrapper>
+          </Paragraph>
+          <Paragraph>You can also find me here: </Paragraph>
+          <SocialLinksRow>
+            <IconLink
+              href="https://github.com/Don-Stevenson"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledGithubIcon />
+            </IconLink>
+            <IconLink
+              href="https://www.linkedin.com/in/don-stevenson-5b31aa128/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StyledLinkedinIcon />
+            </IconLink>
+          </SocialLinksRow>
+        </ContentSection>
+      </ContentWrapper>
     </PageContainer>
   )
 }
