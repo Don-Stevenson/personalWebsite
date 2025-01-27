5fe008524aa2b5717f75c8f82b4d78cde046c075
Don Stevenson <50342473+Don-Stevenson@users.noreply.github.com>
2025-01-27 15:27:13 -0500

    Trigger deploy (#126)
    * Trigger deploy
    
    * Update vite config
    
    * Trigger deploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,15 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["linked-dep"],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      include: [/linked-dep/, /node_modules/],
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
