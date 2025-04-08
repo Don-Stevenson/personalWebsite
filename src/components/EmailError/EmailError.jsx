3201d089b26455fb473caa9a1e310b21501bfd6a
Don Stevenson <dstevenson@tribalscale.com>
2025-04-08 14:55:01 -0400

    Refactor EmailError component, add failed email send message

---
+++ src/components/EmailError/EmailError.jsx
@@ -0,0 +1,9 @@
+import styles from "./EmailError.module.css"
+
+export function EmailError({ errorMsg }) {
+  return (
+    <div className={styles.errorContainer}>
+      {errorMsg && <p className={styles.errorText}>{errorMsg}</p>}
+    </div>
+  )
+}
