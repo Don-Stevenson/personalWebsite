523b234c3827058cef13033ee2cf01005cdbc230
Don Stevenson <dstevenson@tribalscale.com>
2025-08-08 10:42:49 -0400

    WIP: trying to add redirects

--- dist/server/middleware-build-manifest.js
+++ dist/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-2b62440454913efd.js","static/chunks/4bd1b696-cf72ae8a39fa05aa.js","static/chunks/964-e229cbb18e88417e.js","static/chunks/main-app-7fe4fec6118081c9.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-2b62440454913efd.js","static/chunks/framework-f75312fc4004b783.js","static/chunks/main-1a636c02336215de.js","static/chunks/pages/_app-0a0020ddd67f79cf.js"],"/_error":["static/chunks/webpack-2b62440454913efd.js","static/chunks/framework-f75312fc4004b783.js","static/chunks/main-1a636c02336215de.js","static/chunks/pages/_error-03529f2c21436739.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
